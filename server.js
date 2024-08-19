85889df4be316b9dd5ce40dddf28bd882aa21a99
oyejateen <jatinshrimali24@gmail.com>
2024-08-19 19:49:21 +0530

    Server logs added

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.json());
 
 app.use('/uploads', express.static('uploads'));
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
