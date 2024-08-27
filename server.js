eb4bd5acecbb5f840baae2c5ef9acf06f96ad262
oyejateen <jatinshrimali24@gmail.com>
2024-08-27 10:51:03 +0530

    attempt to fix the cors error

--- server.js
+++ server.js
@@ -27,11 +27,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-  ? process.env.CLIENT_URL 
-  : 'http://localhost:3000',
+  origin: process.env.CLIENT_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  credentials: true,
+  optionSuccessStatus: 200
 }));
 app.use((req, res, next) => {
   console.log(`Received ${req.method} request for ${req.url}`);
